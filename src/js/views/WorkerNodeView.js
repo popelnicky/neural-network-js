d5ddc0e106a7f4799fa0ef52dd45cec41089db4b
Dmitriy Popelnitskiy <popelnicky@live.ru>
2023-04-04 12:26:58 +0300

    [chg] Improved worker nodes UI

--- src/js/views/WorkerNodeView.js
+++ src/js/views/WorkerNodeView.js
@@ -1,5 +1,6 @@
 import { NodeState } from "../constants/NodeState.js";
 import { BaseView } from "./BaseView.js";
+import { WorkerNodeIds } from "../constants/WorkerNodeIds.js";
 
 export class WorkerNodeView extends BaseView {
   constructor(id, parent) {
@@ -14,15 +15,38 @@ export class WorkerNodeView extends BaseView {
     this.$view = document.createElement("div");
 
     this.$view.classList.add("worker-node-view");
-    this.$view.classList.add("worker-node-off");
 
-    this.$view.innerHTML = `<div class="${this.id}"></div>`;
+    this.$view.innerHTML = `<label class="worker-node-switch">
+                              <input type="checkbox">
+                              <span class="${this.id} worker-node-slider"></span>
+                            </label>`;
+
+    this.initListeners();
+  }
+
+  initListeners() {
+    this.$view.addEventListener("click", (ev) => {
+      const nodeId = this.findNodeId(ev.target);
+      
+      if(nodeId) {
+        this.toggle();
+      }
+    });
+  }
+
+  findNodeId(target) {
+    for (let item of target.classList) {
+      const nodeId = WorkerNodeIds.find((id) => id === item);
+
+      if (nodeId) {
+        return nodeId;
+      }
+    }
+
+    return null;
   }
 
   toggle() {
-    this.$view.classList.toggle("worker-node-off");
-    this.$view.classList.toggle("worker-node-on");
-
     this.enabled = !this.enabled;
 
     this.parent.sendStateTo(
--- src/js/views/WorkerNodesView.js
+++ src/js/views/WorkerNodesView.js
@@ -21,38 +21,6 @@ export class WorkerNodesView extends BaseView {
 
       this.$view.append(node.view);
     }
-
-    this.initListeners();
-  }
-
-  initListeners() {
-    this.$view.addEventListener("click", (ev) => {
-      if (ev.target === this.$view) {
-        return;
-      }
-
-      const node = this.findNode(ev.target);
-
-      if (!node) {
-        return;
-      }
-
-      node.toggle();
-    });
-  }
-
-  findNode(target) {
-    let nodeId = null;
-
-    for (let item of target.classList) {
-      nodeId = WorkerNodeIds.find((id) => id === item);
-
-      if (nodeId) {
-        break;
-      }
-    }
-
-    return this.nodes.find((item) => item.id === nodeId);
   }
 
   sendStateTo(id, state) {
